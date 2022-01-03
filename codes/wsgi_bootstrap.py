e087e301e03d2ad8d9e3dca5b62e80710262f1f1
Damilola Okuboyejo <d.okuboyejo@gmail.com>
2022-01-04 00:50:28 +0200

    update structure

--- codes/wsgi_bootstrap.py
+++ codes/wsgi_bootstrap.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
 def hello_world():
-    return {'info': 'Hello, World!'}, 200
+    return jsonify({'info': 'Hello, World!'}), 200
 	
 
 if __name__ == '__main__':
